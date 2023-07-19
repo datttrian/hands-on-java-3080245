b9b291de2cf49d65004d7ee190854e12da366163
Dat T. Tran <datttrian@gmail.com>
2023-07-19 06:36:06 +0000

    Create Authenticator

--- src/main/java/bank/DataSource.java
+++ src/main/java/bank/DataSource.java
@@ -14,7 +14,7 @@ public class DataSource {
 
     try {
       connection = DriverManager.getConnection(db_file);
-      System.out.println("We're connected");
+      // System.out.println("We're connected");
     } catch (SQLException e) {
       e.printStackTrace();
     }
@@ -42,8 +42,30 @@ public class DataSource {
     return customer;
   }
 
+  public static Account getAccount(int accountId) {
+    String sql = "select * from accounts where id = ?";
+    Account account = null;
+    try (Connection connection = connect();
+        PreparedStatement statement = connection.prepareStatement(sql)) {
+      statement.setInt(1, accountId);
+
+      try (ResultSet resultSet = statement.executeQuery()) {
+        account = new Account(
+            resultSet.getInt("id"),
+            resultSet.getString("type"),
+            resultSet.getDouble("balance"));
+      }
+    } catch (SQLException e) {
+      e.printStackTrace();
+    }
+
+    return account;
+  }
+
   public static void main(String[] args) {
     Customer customer = getCustomer("twest8o@friendfeed.com");
+    Account account = getAccount(customer.getAccountId());
     System.out.println(customer.getName());
+    System.out.println(account.getBalance());
   }
 }
